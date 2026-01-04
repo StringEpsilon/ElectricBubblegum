13cc6b244e5937fb13c373ee2fb9c8f7fd60f6a8
StringEpsilon <StringEpsilon@gmail.com>
2026-01-04 01:03:42 +0100

    Set video image-rendering.

--- src/interface/PanelLeft/GameTime.tsx
+++ src/interface/PanelLeft/GameTime.tsx
@@ -1,12 +1,13 @@
 import { propertySignal } from "../../functions/propertySignal";
 
 function padNumber(number: Number|undefined) {
-	return (number ?? 0).toString().padStart(2, "0");
+	return (number ?? 0).toFixed(0).padStart(2, "0");
 }
 
 const hours = propertySignal("game_time.hours", padNumber);
 const minutes = propertySignal("game_time.minutes", padNumber);
 const seconds = propertySignal("game_time.seconds", padNumber);
+// const frames = propertySignal<number, string>("game_time.frames", x => padNumber(((x??1)/60*100)) );
 
 export function GameTime() {
 	return (
